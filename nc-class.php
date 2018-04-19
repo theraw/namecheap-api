da6c9251f81168aecd7c18c16f5e3ec42adb8b79
ƬHE ЯAW ☣ <raw@dopehosting.net>
2018-04-20 00:57:06 +0200

    Update nc-class.php

--- nc-class.php
+++ nc-class.php
@@ -16,7 +16,7 @@ class nc {
         curl_setopt($ch, CURLOPT_URL, "$url");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-        curl_setopt($ch, CURLOPT_USERAGENT,'IDK');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_TIMEOUT, 30);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
@@ -25,18 +25,22 @@ class nc {
         $xmlR = simplexml_load_string($res);
         $thisJS = json_encode($xmlR);
         $decJS = json_decode($thisJS, true);
-        // echo $decJS['CommandResponse']['DomainCheckResult']['@attributes']['Available'];
-        $status = $decJS['@attributes']['Status'];
-        
-        if ($status == 'OK') {
-            $check = $decJS['CommandResponse']['DomainCheckResult']['@attributes']['Available'];
-             if ($check == 'false') {
-                 echo "Taken.";
-             } else {
-                 echo "Available";
-             }
+
+        $ipr = $decJS['CommandResponse']['DomainCheckResult']['@attributes']['IsPremiumName'];
+        if ($ipr = 'false') {
+            $status = $decJS['@attributes']['Status'];
+            if ($status == 'OK') {
+                $check = $decJS['CommandResponse']['DomainCheckResult']['@attributes']['Available'];
+                 if ($check == 'false') {
+                     echo "Taken.";
+                 } else {
+                     echo "Available";
+                 }
+            } else {
+                echo "Err Not Available";
+            }
         } else {
-            echo "Err Not Available";
+            echo "This is a premium domain please contact support to register it.";
         }
         
     }
